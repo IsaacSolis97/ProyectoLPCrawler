28d6205f93898f54454fe3d4fcd35d58c0d3f6fe
FranciscoBasantes <fxbasant@espol.edu.ec>
2021-02-01 23:28:19 -0500

    captures

--- Scrapper_Basantes/CodigoPython/analizador.py
+++ Scrapper_Basantes/CodigoPython/analizador.py
@@ -1,20 +1,32 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#crea el dataframe usando pandas
 ligas = pd.read_csv ("./../CodigoRuby/datos.csv")
+
+#borra elementos nan
 ligas.dropna()
+
+#nombra columnas
 ligas.columns = ["Rank", "Squad", "MatchesPlayed", "Wins", "Draws", "Losses", "Points", "GoalDiference"]
 
 print("Analisis y estadisticas sobre los diferentes equipos en sus respectivas ligas")
 
+#genera lista de equipos no repetidos
 equipos = ligas['Squad'].unique().tolist()
 cantidad = len(equipos)
 print(equipos)
 print("La cantidad de equipos diferentes en el mundo del futbol es: ", cantidad)
 
+descriptor = ligas['Points'].describe()
+print("Datos estadisticos significativos para el analisis")
+print(descriptor)
 
-top5 = ligas.sort_values(by=['Points']).tail(6)
-top5.drop(top5.tail(1).index,inplace=True)
+#genera grafico
 ligas['Points'].plot(kind='bar')
 plt.show()
+
+#genera top 5 en puntaje
+top5 = ligas.sort_values(by=['Points']).tail(6)
+top5.drop(top5.tail(1).index,inplace=True)
 print(top5)
