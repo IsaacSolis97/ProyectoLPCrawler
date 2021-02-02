1b9d7cbd5661c04965bbd42bd9a3a6b9251a81e2
FranciscoBasantes <fxbasant@espol.edu.ec>
2021-02-01 22:55:02 -0500

    captures + analisis

--- Scrapper_Basantes/CodigoPython/analizador.py
+++ Scrapper_Basantes/CodigoPython/analizador.py
@@ -1,5 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+ligas = pd.read_csv ("./../CodigoRuby/datos.csv")
+ligas.dropna()
+ligas.columns = ["Rank", "Squad", "MatchesPlayed", "Wins", "Draws", "Losses", "Points", "GoalDiference"]
 
-print("hola mundo")
+print("Analisis y estadisticas sobre los diferentes equipos en sus respectivas ligas")
+
+equipos = ligas['Squad'].unique().tolist()
+cantidad = len(equipos)
+print(equipos)
+print("La cantidad de equipos diferentes en el mundo del futbol es: ", cantidad)
+
+
+top5 = ligas.sort_values(by=['Points']).tail(6)
+top5.drop(top5.tail(1).index,inplace=True)
+ligas['Points'].plot(kind='bar')
+plt.show()
+print(top5)
