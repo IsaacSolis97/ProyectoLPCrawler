74508ebe0993d6df5c17b95d9d73228e293b9945
FranciscoBasantes <fxbasant@espol.edu.ec>
2021-02-02 22:49:59 -0500

    nuevos captures

--- Scrapper_Basantes/CodigoPython/analizador.py
+++ Scrapper_Basantes/CodigoPython/analizador.py
@@ -13,20 +13,31 @@ ligas.columns = ["Rank", "Squad", "MatchesPlayed", "Wins", "Draws", "Losses", "P
 print("Analisis y estadisticas sobre los diferentes equipos en sus respectivas ligas")
 
 #genera lista de equipos no repetidos
-equipos = ligas['Squad'].unique().tolist()
-cantidad = len(equipos)
-print(equipos)
-print("La cantidad de equipos diferentes en el mundo del futbol es: ", cantidad)
+#equipos = ligas['Squad'].unique().tolist()
+#cantidad = len(equipos)
+#print(equipos)
+#print("La cantidad de equipos diferentes en el mundo del futbol es: ", cantidad)
 
 descriptor = ligas['Points'].describe()
+media = ligas['Points'].mean()
 print("Datos estadisticos significativos para el analisis")
 print(descriptor)
+print("media aritmetica: ", media)
+
+maximo = ligas['Points'].max()
+diferencia = maximo - media
+print("La maxima cantidad de puntos es: ", maximo)
+print("La diferencia entre el maximo y la media aritmetica es: ", diferencia)
+print("Los equipos que exeden 62 puntos son: ")
+print(ligas[ligas.Points > 62])
+
+
 
 #genera grafico
-ligas['Points'].plot(kind='bar')
-plt.show()
+#ligas['Points'].plot(kind='bar')
+#plt.show()
 
 #genera top 5 en puntaje
-top5 = ligas.sort_values(by=['Points']).tail(6)
-top5.drop(top5.tail(1).index,inplace=True)
-print(top5)
+#top5 = ligas.sort_values(by=['Points']).tail(6)
+#top5.drop(top5.tail(1).index,inplace=True)
+#print(top5)
